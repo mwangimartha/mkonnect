f5050f31497c1d1d3b27d87e7b680b7442df0a8f
movavikim <jimhawkinskim@gmail.com>
2017-06-28 12:45:57 +0300

    test username

--- achievements.php
+++ achievements.php
@@ -2,8 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	
-	<title>test edit for git</title>
+		<title>test username</title>
        <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
