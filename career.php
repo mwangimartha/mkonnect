65dd4a584860abb68ade9de85830e12b829931d2
movavikim <jimkimathi@uxt.co.ke>
2017-06-28 10:12:06 +0300

    initial commit careers page

---
+++ career.php
@@ -0,0 +1,198 @@
+<! DOCTYPE HTML>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>JMkonnect</title>
+       <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" >
+    <meta name="description" content="WOWSlider">
+    <meta name="google-site-verification" content="QapaVeCTXy4MLxEILztDixJjvPsF7NpjUdP1do_qhYg">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta name="HandheldFriendly" content="true">
+    <meta name="HandheldFriendly" content="true">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+    <link rel="canonical" href="https://www.mkonnect.co.ke" >
+     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/stylemkonect.css">
+    <link rel="stylesheet" type="text/css" href="css/materialize.css">
+    <link rel="stylesheet" type="text/css" href="css/materialize.min.css">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/site.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.1/css/materialize.min.css">
+    <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!--w3 css-->
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet"> 
+    <link rel="stylesheet" type="text/css" href="css/stylemkonect.css">
+    <link rel="stylesheet" type="text/css" href="css/materialize/css/materialize.css"> 
+    <link rel="stylesheet" type="text/css" href="css/materialize-min.css"> 
+    <link rel="stylesheet" type="text/css" href="css/stylemkonect.css"> 
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="jobseeker.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+   <link rel="stylesheet" type="text/css" href="css/materialize/css/materialize.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.js"></script>
+   <script type="text/javascript" src="js/jquerymkon.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+   
+</head>
+<body style="background-color: #f5f5f5;" >
+
+       <nav  class="navbar navbar-default navbar-fixed-top" style="background-color: #0d47a1;">
+      <!--this is to collapse the nav bar menu on small screens-->
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <!--this is to collapse the nav bar menu on small screens-->
+      <div class="collapse navbar-collapse" id="myNavbar">
+      
+         <ul style="text-align:center; background-color:#0d47a1 ; ">
+          
+           <div class="job">
+          
+            <li><a href="index.html">HOME</a></li>
+            <li><a href="#">MY APPLICATIONS</a></li>
+            <li style="" class="home"><a href="#">MY CV</a></li>
+              <li><a href="#">MESSAGES</a></li>
+            <li><a href="#">EMPLOYER</a></li>
+
+            </div>
+            
+            
+            
+
+            
+
+             <a href="jobseeker.html" style="background-color: #f57c00; box-shadow: 5px;" class="btn btn-info" role="button">LOG OUT</a>
+
+            
+         </ul>
+         </div>
+        
+    
+      </nav>
+
+      <div class="row">
+      <center><h4 style="color:  #f57c00; font-weight: bold; font-family: franklin gothic heavy; margin-top: 95px;">WELCOME TO MKONNECT, YOUR CAREER PAGE</h4></center>
+      </div>
+
+      
+        <div style="margin-top: 20px; background-color: #fff;" class="row">
+
+        
+         
+
+         
+
+         </div>
+        
+
+        
+
+
+       <div class="col-sm-9">
+
+      <!--bootstrap css registration form-->
+
+
+       <div style="height: auto; width: auto; background-color: #fff; box-shadow: 5px 5px #e0e0e0; margin-top: 250px; margin-left: 0px;" class="container">
+       
+       
+   
+  <form>
+
+    <div style="margin-top: -200px;" class="form-group">
+   
+      <label style="color: #000; font-weight: bold; font-size: 20px; font-family: franklin gothic heavy;" for="comment">Write a brief summary about your career:</label>
+      <textarea style="background-color: #fff; height: 250px;" class="form-control" rows="5" id="comment"></textarea>
+
+      <center><button style="margin-top: 20px; background-color: #f57c00;" type="submit" name="save" class="btn btn-default">Save</button></center>
+  </form>
+
+
+    </div>
+  </form>
+</div>
+</div>
+
+ <footer style="background-color:#0d47a1; ">
+         <div  class="row">
+
+
+         
+            <div class="col m3">
+          <h6 style="margin-left: 10px;"> Our Mission & Vision</h6>
+
+          <p style="color: #9e9e9e;" class="para1">At mkonnect we link the employer and the job
+             seeker. Profile matching employer and job
+             seekers according to employer's requirements
+             and the job seekers qualifications.
+          </p>
+
+               
+
+            </div>
+
+
+           <div class="col m3">
+
+            <h6>Find Us On:</h6>
+
+          <img src="images/facebook.png" style="width: 30px; height: 30px;"></br>
+          <img src="images/google.png" style="width: 30px; height: 30px;"></br>
+          <img src="images/twitter.jpg" style="width: 30px; height: 30px;"></br>
+          <img src="images/linkedin.png" style="width: 30px; height: 30px;"></br>
+            </div>
+
+            <div class="col m3">
+
+            <h6>Important Links</h6>
+
+          <a href="mkonnect/terms_conditions.php" style="color: #9e9e9e; line-height: 30px;">Terms and Conditions</a></br>
+          <a href="mkonnect/privacy_policy.php" style="color: #9e9e9e; line-height: 30px;">Policy & Privacy</a></br>
+            <a href="#" style="color: #9e9e9e; line-height: 30px;">FAQs</a>
+            
+            
+            </div>
+
+            <div class="col m3">
+
+            <h6>Contact Info:</h6>
+
+            <img src="images/location.png" style="width: 30px; height: 30px;"></br>
+            <img src="images/phone.png" style="width: 30px; height: 30px;"></br>
+            <img src="images/email.jpg" style="width: 30px; height: 30px;"></br>
+            <img src="images/twitter.jpg" style="width: 30px; height: 30px;"></br>
+
+            </div>
+
+        </div>
+
+     </footer>
+
+
+
+        
+
+        </body>
+        </html>
